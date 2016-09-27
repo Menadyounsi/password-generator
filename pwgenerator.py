acebe4bfcdb4aedcd5dce688fa8c974eea7f00f8
Menadyounsi <alanyounes1@gmail.com>
2016-09-27 12:43:19 +0100

    password generator
    This script can generate random passwords of user decided length, and as
    many as the user needs

---
+++ pwgenerator.py
@@ -0,0 +1,39 @@
+#Password generator
+import random
+import string
+
+#set the characters to choose from for Password
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@&*()"
+print "Hello and welcome to Menad's random password generator\n\n"
+
+length = int(raw_input("How many characters in length would you like the password:  "))
+
+quantity = int(raw_input("How many passwords do you need generated? :   "))
+
+file = open("passwords.txt", "a")
+
+final_output = ""
+
+
+def Pwgenerate(numchar):
+    lenchar = 0
+
+    password = random.sample(characters, numchar)
+    output = ''.join(password)
+    file.write(output + "\n")
+    print output
+
+
+print "Hello and welcome to Menad's random password generator"
+
+def Number(quantity, length):
+    count = 0
+    while count != quantity:
+        Pwgenerate(length)
+        count += 1
+
+    file.close()
+
+
+Number(quantity, length)
+file.close()
